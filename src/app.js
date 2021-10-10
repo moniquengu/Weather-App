dcbce0084e779ab08ede92821a2837e4468b9c12
moniquengu <87856871+moniquengu@users.noreply.github.com>
2021-10-10 22:28:57 +1000

    Link JS

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ let apiKey = "fdc8a450df947256bc83a4a53890637a";
 let apiCity = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=fdc8a450df947256bc83a4a53890637a&units=metric`;
 
 function displayWeatherStats(response) {
-  console.log(response.data.main);
+  console.log(response.data);
 }
 
 axios.get(apiCity).then(displayWeatherStats);
