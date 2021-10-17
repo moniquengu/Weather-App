d4a18e7fb00bfbe6f2a7520ec767a1df8c0c6889
moniquengu <87856871+moniquengu@users.noreply.github.com>
2021-10-17 23:47:08 +1000

    Add JS Show City from search

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 let apiKey = "fdc8a450df947256bc83a4a53890637a";
-let apiCity = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=fdc8a450df947256bc83a4a53890637a&units=metric`;
 
-function displayWeatherStats(response) {
-  console.log(response.data);
+//show city #1
+function showCity(response) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-search");
+  let currentCity = cityInput.value;
+  let newCity = document.querySelector("#city-location");
+  newCity.innerHTML = `${currentCity}`;
+  let apiCity = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=fdc8a450df947256bc83a4a53890637a&units=metric`;
 }
-
-axios.get(apiCity).then(displayWeatherStats);
+let cityInput = document.querySelector("#city-search-bar");
+cityInput.addEventListener("submit", showCity);
